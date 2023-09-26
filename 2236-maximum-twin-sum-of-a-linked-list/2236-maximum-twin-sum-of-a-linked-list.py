75147ba552cfc21a0dcbdba9e1da0515193a6c85
Tiela Rose Black-Law <26930264+tielarose@users.noreply.github.com>
2023-09-26 14:35:53 -0700

    Time: 576 ms (90.99%), Space: 37.8 MB (99.79%) - LeetHub

--- 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.py
+++ 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.py
@@ -5,51 +5,32 @@
 #         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        # Step 1
-        # find the middle of the linked list using a fast and slow pointer
-        # ex: if the list was [1,2,3,4,5,6], find 4
-        prev = None
-        fast = slow = head
-
-        while fast and fast.next:
-            prev = slow
-            slow = slow.next
-            fast = fast.next.next
-
-        # slow is now the middle of the linked list (4 in my example)
-        # prev is now the last node of the unreversed part of the list (3 in my example)
-
-        # Step 2
-        # reverse the linked list from the middle 
-        # ex: [1,2,3,6,5,4]
-        last_node = prev # save to link the reversed part of the list with the unreversed part of the list (ie so [1,2,3] can be linked with [6,5,4])
-
-        prev = None
-        curr = slow
-
-        while curr:
-            next_node = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next_node
-
-        last_node.next = prev
-
-        # the second-half of the linked list is now reversed
-        # "prev" is at the middle node (now 6)
-
-        # Step 3
-        # loop through the list with one pointer at the beginning, one starting from the second-half (aka "prev"). Find the max sume of these pairs
-
+        slow, fast, prev, curr = head, head, None, head
         max_sum = 0
-        first = head
-        second = prev
 
-        while second:
-            max_sum = max(first.val + second.val, max_sum)
-            first = first.next
-            second = second.next
+        # reverse the first half of the linked list
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+            curr.next, prev, curr = prev, curr, slow
+
+        # sum the pairs of nodes to find the max
+        while slow:
+            max_sum = max(max_sum, prev.val + slow.val)
+            slow, prev = slow.next, prev.next
 
         return max_sum
+
+# [1,2,3,4]
+
+# slow = 1
+# fast = 1
+# (fast.next = 2)
+# prev = 1
+# curr = 2
+# max_sum = 0
+            
+
+
+
 
         
