af0083968a49216fbbab73d11deed0b71d71e859
Tiela Rose Black-Law <26930264+tielarose@users.noreply.github.com>
2023-09-28 15:39:46 -0700

    Time: 727 ms (40.74%), Space: 50.6 MB (73.59%) - LeetHub

--- 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.py
+++ 0528-swapping-nodes-in-a-linked-list/0528-swapping-nodes-in-a-linked-list.py
@@ -8,42 +8,21 @@ class Solution:
         dummy = ListNode(0, head)
 
         # find the node k nodes from the beginning
-        before_A = dummy
         node_A = head
 
         for i in range(1,k):
-            before_A = before_A.next
             node_A = node_A.next
 
-        after_A = node_A.next
-
         # find the node k nodes from the end
-        before_B = dummy
         node_B = head
         fast = node_A
 
         while fast and fast.next:
-            before_B = before_B.next
             node_B = node_B.next
             fast = fast.next
 
-        after_B = node_B.next
-
-        # switch the pointers
-        # check edge case where the two nodes to be switched are next to each other
-        if after_A == node_B:
-            before_A.next = node_B
-            node_B.next = node_A
-            node_A.next = after_B
-        elif after_B == node_A:
-            before_B.next = node_A
-            node_A.next = node_B
-            node_B.next = after_A
-        else:
-            before_A.next = node_B
-            node_B.next = after_A
-            before_B.next = node_A
-            node_A.next = after_B
+        # switch the values
+        node_B.val, node_A.val = node_A.val, node_B.val
 
         return dummy.next
 
