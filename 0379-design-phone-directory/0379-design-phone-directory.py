c6eff7156dc09c1dd4395dd7954f96a2c45c91fa
Tiela Rose Black-Law <26930264+tielarose@users.noreply.github.com>
2023-09-30 14:16:13 -0700

    Time: 200 ms (21.99%), Space: 20.1 MB (91.27%) - LeetHub

--- 0379-design-phone-directory/0379-design-phone-directory.py
+++ 0379-design-phone-directory/0379-design-phone-directory.py
@@ -1,21 +1,19 @@
 class PhoneDirectory:
 
     def __init__(self, maxNumbers: int):
-        self.maxNumbers = maxNumbers
-        self.directory = [[]] * maxNumbers
+        self.is_slot_available = [True] * maxNumbers
 
     def get(self) -> int:
-        for i in range(self.maxNumbers):
-            if not self.directory[i]:
-                self.directory[i] = 'taken'
-                return i
-        return -1
+        index = next((ind for ind, is_available in enumerate(self.is_slot_available) if is_available), -1)
+        if index != -1:
+            self.is_slot_available[index] = False
+        return index
 
     def check(self, number: int) -> bool:
-        return self.directory[number] == []
+        return self.is_slot_available[number]
         
     def release(self, number: int) -> None:
-        self.directory[number] = []
+        self.is_slot_available[number] = True
         
 
 
