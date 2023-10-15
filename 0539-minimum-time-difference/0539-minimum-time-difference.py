d6af4e6ec04ccb6cc1868d38e23630c5749d03ae
Tiela Rose Black-Law <26930264+tielarose@users.noreply.github.com>
2023-10-15 13:26:07 -0700

    Time: 65 ms (97.06%), Space: 19.6 MB (40.77%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.py
+++ 0539-minimum-time-difference/0539-minimum-time-difference.py
@@ -1,31 +1,26 @@
 class Solution:
     def findMinDifference(self, timePoints: List[str]) -> int:
-        def convert_to_minutes(time_point):
-            """Takes in a time in format "HH:MM",
-            returns time in minutes from midnight""" 
-            hrs = int(time_point[:2])
-            mins = int(time_point[3:])
+        # convert the times to minutes
+        def convert_time_to_min(time_str):
+            hours = int(time_str[:2])
+            mins = int(time_str[3:])
+            return (hours * 60) + mins
 
-            return (hrs * 60) + mins
+        times_list = [convert_time_to_min(x) for x in timePoints]
 
-        # convert all times to num of minutes
-        times_in_min = [convert_to_minutes(x) for x in timePoints]
+        # sort the list
+        times_list.sort()
 
-        # sort the list in ascending order
-        times_in_min.sort()
-
-        # handle edge case: add 24 hours to the first time, in case it's closer to the last time. For example, 23:59 and 00:00 are only one minute apart, but when coverted to minutes, they seem very far apart (1439 and 0); adding 1440 to the smaller time (0) accounts for this "crossing midnight" (1439 and 1440 are correctly calculated as only one minute apart)
-        times_in_min.append(times_in_min[0] + 1440)
-
-        # set the min diff to larger than the max possible difference
-        min_diff = 1441
-
-        # iterate through the sorted times, comparing each pair to the current min
-        for i in range(len(times_in_min) - 1):
-            curr_time = times_in_min[i]
-            next_time = times_in_min[i+1]
-
-            min_diff = min(min_diff, next_time - curr_time)
-
-        return min_diff
+        # handle the edge case of times that cross midnight
+        times_list.append((24 * 60) + times_list[0])
         
+        # loop through the list, comparing times
+        min_time = (24 * 60) + 1
+
+        for i in range(len(times_list) - 1):
+            curr_time = times_list[i]
+            next_time = times_list[i + 1]
+
+            min_time = min(min_time, next_time - curr_time)
+
+        return min_time
